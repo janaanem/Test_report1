5333d37f6d7fb0f698e2b02dca30d800959a0ffc
janaanem <janardhanaraoanem@gmail.com>
2025-06-27 15:39:27 +0530

    Create DataFramePandas.py

---
+++ DataFramePandas.py
@@ -0,0 +1,4 @@
+import pandas as pd
+data={"rollno":[1,2,3,4,5,6,7,8,9,10],"name":["vamshi","ravi","jana","srinu","abhi","teja","ajith","raja","snehith","adhi"],"section":["apollo","crypto","apollo","crypto","apollo","crypto","apollo","crypto","apollo","crypto"]}
+df=pd.DataFrame(data)
+print(df)
